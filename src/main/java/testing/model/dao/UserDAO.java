301e98c5242b183d1f8bf01cbd40aca540ed509d
Jan Semacio <gsbirth@gmail.com>
2018-09-02 16:10:40 +0800

    use persistence context anotation

--- src/main/java/testing/model/dao/UserDAO.java
+++ src/main/java/testing/model/dao/UserDAO.java
@@ -1,47 +1,36 @@
 package testing.model.dao;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 
 import testing.model.entity.User;
 
 public class UserDAO implements IUserDAO {
 
-	private String persistenceUnit = "PostgreDS";
-	private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnit);
+	@PersistenceContext
+	EntityManager entityManager;
 
 	public User getById(int id) {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		User user = entityManager.find(User.class, id);
-		entityManager.close();
-		return user;
+		return entityManager.find(User.class, id);
 	}
 
 	public void add(User user) {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 		entityManager.persist(user);
 		entityManager.getTransaction().commit();
-		entityManager.close();
 	}
 
 	public void update(User user) {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 		entityManager.merge(user);
 		entityManager.getTransaction().commit();
-		entityManager.close();
 	}
 
 
 	public void delete(int id) {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		User user = entityManager.find(User.class, id);
 		entityManager.getTransaction().begin();
 		entityManager.remove(user);
 		entityManager.getTransaction().commit();
-		entityManager.close();
 	}
-
 }
